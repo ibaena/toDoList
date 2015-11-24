3cdf79d75d8084a23d2c28b06247a4752245bd7e
Ivan <ivan.baena04@gmail.com>
2015-11-24 04:37:37 -0500

    edit styling

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-
+//Pass information from form to table
   $('.addtask').click(function(e) {
     e.preventDefault();
 
@@ -9,35 +9,35 @@ $(document).ready(function() {
     var taskDescription = $('.description').val();
     var taskDate = $('.date').val();
     var newRow = $("<tr>");
-    var wordTd = $("<td>").append(inputTask + "<br>" + taskDescription + "<br>" +taskDate +"</td>");
+    var wordTd = $("<td>").append(inputTask +"&nbsp;"+"&nbsp;"+"-  "+ taskDate +"<hr>" + taskDescription +"</td>");
     var deleteBtn = $("<button>").append("Delete");
     var deleteTd = $("<td>").append(deleteBtn);
     var completeBtn = $("<input>").append("Complete")
     var completeTd = $("<td>").append(completeBtn);
 
-    //style buttons and checkbox
+//Add classs and attriute to style buttons and checkbox
     deleteBtn.addClass("btn btn-danger");
     completeBtn.attr('type', 'checkbox');
     completeBtn.attr('value', 'FALSE');
     completeBtn.addClass("checkBox");
 
-    //Add content to table
+//Add content to table
     newRow.append(wordTd);
     newRow.append(completeTd);
     newRow.append(deleteTd);
     $("tbody").append(newRow);
 
-    //Clear input text afetr submit
+//Clear input text afetr submit
     $(".taskTitle,.description,.date").val("");
     // alert("hey do i work");
   });
 
-  //delete button function
+//delete button function
   $("table").on("click", ".btn-danger", function() {
     $(this).parent().parent().remove();
   });
 
-  //checkbox to create strikethrough and then remove   
+//checkbox to create strikethrough and then remove   
   $("table").on("click", ".checkBox", function(e) {
     e.preventDefault();
     //$('.checkBox').prop('checked',".checkBox");
@@ -46,8 +46,14 @@ $(document).ready(function() {
     $(this).remove();
   });
 
+//chage button text
+$(function(){
+   $("#open").click(function () {
+      $(this).text(function(i, text){
+          return text === "Add a Task" ? "Close" : "Add a Task";
+      })
+   });
+})
 
-
-
-
+    
 });
