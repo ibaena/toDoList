8a3801b83bce8ba48ba618a855cf3706bb16c137
Ivan <ivan.baena04@gmail.com>
2015-11-24 06:30:27 -0500

    added datepicker to input and updated css

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-
-//Pass information from form to table
+  //Pass information from form to table
   $('.addtask').click(function(e) {
     e.preventDefault();
 
@@ -9,35 +8,40 @@ $(document).ready(function() {
     var taskDescription = $('.description').val();
     var taskDate = $('.date').val();
     var newRow = $("<tr>");
-    var wordTd = $("<td>").append(inputTask +"&nbsp;"+"&nbsp;"+"-  "+ taskDate +"<hr>" + taskDescription +"</td>");
-    var deleteBtn = $("<button>").append("Delete");
+    var wordTd = $("<td>").append("<div class='title'>" + inputTask + "&nbsp;" + "&nbsp;" + "-  " + taskDate + "</div>" + taskDescription + "</td>");
+    var deleteBtn = $("<button>").append("<span class='glyphicon glyphicon-trash'></span>");
     var deleteTd = $("<td>").append(deleteBtn);
     var completeBtn = $("<input>").append("Complete")
     var completeTd = $("<td>").append(completeBtn);
 
-//Add classs and attriute to style buttons and checkbox
+    //Add classs and attriute to style buttons and checkbox
     deleteBtn.addClass("btn btn-danger");
     completeBtn.attr('type', 'checkbox');
     completeBtn.attr('value', 'FALSE');
     completeBtn.addClass("checkBox");
 
-//Add content to table
-    newRow.append(wordTd);
-    newRow.append(completeTd);
-    newRow.append(deleteTd);
-    $("tbody").append(newRow);
+    //logic for empty form values
+    if (inputTask == "" || taskDescription == "" || taskDate == "") {
+      alert("For Done to really help organize your life. You must fill out all the fields!");
+    } else {
+      //Add content to table
+      newRow.append(wordTd);
+      newRow.append(completeTd);
+      newRow.append(deleteTd);
+      $("tbody").append(newRow);
 
-//Clear input text afetr submit
-    $(".taskTitle,.description,.date").val("");
-    // alert("hey do i work");
+      //Clear input text afetr submit
+      $(".taskTitle,.description,.date").val("");
+      // alert("hey do i work");
+    };
   });
 
-//delete button function
+  //delete button function
   $("table").on("click", ".btn-danger", function() {
     $(this).parent().parent().remove();
   });
 
-//checkbox to create strikethrough and then remove   
+  //checkbox to create strikethrough and then remove itself 
   $("table").on("click", ".checkBox", function(e) {
     e.preventDefault();
     //$('.checkBox').prop('checked',".checkBox");
@@ -46,14 +50,18 @@ $(document).ready(function() {
     $(this).remove();
   });
 
-//chage button text
-$(function(){
-   $("#open").click(function () {
-      $(this).text(function(i, text){
-          return text === "Add a Task" ? "Close" : "Add a Task";
+  //change addtask button text when collapsing
+  $(function() {
+    $("#open").click(function() {
+      $(this).text(function(i, text) {
+        return text === "Add a Task" ? "Close" : "Add a Task";
       })
-   });
-})
+    });
+  });
 
-    
+  //datepicker
+  $(function() {
+    $(".date").datepicker();
+  });
+
 });
